41b3ccc67697fcc917beb61ff25b8aca92ddee20
Pedram Pejman <pedrampejman2010@gmail.com>
2019-08-07 22:08:41 -0700

    Adds providers/ clients/ & Gopkg files

---
+++ providers/calendar/README.md
@@ -0,0 +1,3 @@
+*Adapted from github.com/sethvargo/terraform-provider-googlecalendar
+
+`examples/event.tf` creates a demo Google Calendar event via a Terraform resource.
---
+++ providers/calendar/examples/event.tf
@@ -0,0 +1,15 @@
+resource "calendar_event" "peddy_kyle" {
+  summary     = "Sit around and shoot shit"
+  description = "See summary"
+  location    = "Fort Asshole"
+
+  start = "2019-07-07T13:00:00-05:00"
+  end   = "2019-07-07T14:00:00-05:00"
+
+  attendee {
+    email = "pedrampejman2010@gmail.com"
+  }
+  attendee {
+    email = "kylevonbredow@gmail.com"
+  }
+}
